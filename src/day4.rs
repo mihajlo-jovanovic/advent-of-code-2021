4b283c6dc8f6f8a0196e7cdd7b4b7a349ab5b294
Mihajlo Jovanovic <mihajlo@mihajlos-mbp.lan>
2021-12-04 22:30:18 -0500

    day 4 alternate solution: slower but more functional and arguably more elegant as well

--- src/day4.rs
+++ src/day4.rs
@@ -1,104 +1,68 @@
-use std::collections::HashSet;
-
-#[derive(Debug, Clone)]
-struct Board {
-    row_cols: Vec<HashSet<u8>>
-}
-
-impl Board {
-    fn check_bingo(&self, nums: &HashSet<u8>) -> bool {
-        for n in &self.row_cols {
-            if n.is_subset(nums) {
-                return true;
-            }
-        }
-        false
-    }
-    fn sum_nonmatching_numbers(&self, nums: &HashSet<u8>) -> u32 {
-        let mut acc: u32 = 0;
-        for i in self.row_cols.iter().take(BOARD_SIZE) {
-            acc += i.iter().filter(|&x| !nums.contains(x)).cloned().map(u32::from).sum::<u32>()
-        }
-        acc
-    }
-}
+use array2d::Array2D;
 
 const BOARD_SIZE: usize = 5;
 
+#[derive(Debug, Clone)]
+struct Board {
+    data: Array2D<u8>,
+}
+
+impl Board {
+    fn new(element: &[u8]) -> Board {
+        Board { data: Array2D::from_row_major(element, BOARD_SIZE, BOARD_SIZE) }
+    }
+
+    // Returns a tuple of the index when this board would be a winner in the first position and its `score` in the second position
+    fn play(&self, nums: &[u8]) -> Option<(usize, u16)> {
+        (5..nums.len()).find_map(|i| {
+            self.data.as_rows().iter().chain(self.data.as_columns().iter()).find_map(|r| {
+                if r.iter().filter(|&e| !nums[..i].contains(e)).count() == 0 {
+                    let score: u16 = self.data.as_row_major().iter().filter(|&x| !nums[..i].contains(x)).cloned().map(u16::from).sum();
+                    return Some((i - 1, score));
+                }
+                None
+            })
+        })
+    }
+}
+
+struct Game {
+    numbers: Vec<u8>,
+    boards: Vec<Board>,
+}
+
 #[aoc_generator(day4)]
-fn generator_input(input: &str) -> (Vec<u8>, Vec<Vec<u8>>) {
-    let nums = input.lines().next().unwrap().split(',').map(|n| n.parse().unwrap()).collect();
-    let a = input.lines().skip(1).filter(|l| !l.is_empty()).map(|l| l.split_whitespace().map(|n| n.parse().unwrap()).collect()).collect();
-    (nums, a)
+fn generator_input(input: &str) -> Game {
+    let (n, b) = input.split_once("\n\n").unwrap();
+    Game {
+        numbers: n.split(',').map(|n| n.parse().unwrap()).collect(),
+        boards: b.split("\n\n")
+            .map(|l| {
+                let a = l.split_whitespace();
+                let b: Vec<u8> = a.map(|s| s.parse().unwrap()).collect();
+                Board::new(&b)
+            })
+            .collect(),
+    }
 }
 
 #[aoc(day4, part1)]
-fn part1(input: &(Vec<u8>, Vec<Vec<u8>>)) -> Option<u32> {
-    let boards = parse_boards(&input.1);
-    let mut bingo: HashSet<u8> = input.0.iter().take(BOARD_SIZE).cloned().collect();
-    for i in BOARD_SIZE..input.0.len() {
-        for b in &boards {
-            if b.check_bingo(&bingo) {
-                println!("found one: {:?}", b);
-                println!("last number drawn: {:?}", input.0.get(i - 1).unwrap());
-                println!("sum non matching: {:?}", b.sum_nonmatching_numbers(&bingo));
-                return Some(*input.0.get(i - 1).unwrap() as u32 * b.sum_nonmatching_numbers(&bingo));
-            }
-        }
-        bingo.insert(*input.0.get(i).unwrap());
-    }
-    None
+fn part1(input: &Game) -> u16 {
+    let winner = input.boards.iter().map(|b| b.play(&input.numbers).unwrap()).min().unwrap();
+    *input.numbers.get(winner.0).unwrap() as u16 * winner.1
 }
 
 #[aoc(day4, part2)]
-fn part2(input: &(Vec<u8>, Vec<Vec<u8>>)) -> Option<u32> {
-    let mut boards = parse_boards(&input.1);
-    let mut bingo: HashSet<u8> = input.0.iter().take(BOARD_SIZE).cloned().collect();
-    for i in BOARD_SIZE..input.0.len() {
-        if boards.len() == 1 {
-            let b = boards.last().unwrap();
-            if b.check_bingo(&bingo) {
-                println!("found one: {:?}", b);
-                let last = input.0.get(i - 1).unwrap();
-                println!("last number drawn: {}", last);
-                println!("sum non matching: {:?}", b.sum_nonmatching_numbers(&bingo));
-                return Some(*last as u32 * b.sum_nonmatching_numbers(&bingo));
-            }
-        }
-        // filter out those boards that are already matching
-        boards = boards.iter().filter(|&b| !b.check_bingo(&bingo)).cloned().collect();
-        // move to the next bingo number
-        bingo.insert(*input.0.get(i).unwrap());
-    }
-    None
+fn part2(input: &Game) -> u16 {
+    let winner = input.boards.iter().map(|b| b.play(&input.numbers).unwrap()).max().unwrap();
+    *input.numbers.get(winner.0).unwrap() as u16 * winner.1
 }
 
-fn parse_boards(input: &[Vec<u8>]) -> Vec<Board> {
-    let mut boards: Vec<Board> = vec![];
-    for board_idx in 0..input.len() / BOARD_SIZE {
-        let mut nums: Vec<HashSet<u8>> = vec![];
-        //rows
-        for i in 0..BOARD_SIZE {
-            let row = input.get(board_idx * BOARD_SIZE + i).unwrap();
-            nums.push(row.iter().cloned().collect());
-        }
-        //cols
-        let mut cols: Vec<u8> = vec![];
-        for i in 0..BOARD_SIZE {
-            for j in 0..BOARD_SIZE {
-                let row = input.get(board_idx * 5 + j).unwrap();
-                cols.push(*row.get(i).unwrap());
-            }
-        }
-        nums.extend(cols.chunks(5).map(|c| c.iter().cloned().collect()).collect::<Vec<HashSet<u8>>>());
-        boards.push(Board { row_cols: nums })
-    }
-    boards
-}
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-#[test]
-fn test_parse_input() {
-    let input = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
+    const INPUT: &str = "7,4,9,5,11,17,23,2,0,14,21,24,10,16,13,6,15,25,12,22,18,20,8,19,3,26,1
 
 22 13 17 11  0
  8  2 23  4 24
@@ -117,18 +81,39 @@ fn test_parse_input() {
 18  8 23 26 20
 22 11 13  6  5
  2  0 12  3  7";
-    let parsed = generator_input(input);
-    println!("{:?}", parsed);
-    assert_eq!(27, parsed.0.len());
-    assert_eq!(15, parsed.1.len());
-    let boards = parse_boards(&parsed.1);
-    assert_eq!(3, boards.len());
-    for b in boards {
-        assert_eq!(10, b.row_cols.len());
-        for nums in b.row_cols {
-            assert_eq!(5, nums.len());
+
+    #[test]
+    fn test_parse_input() {
+        let input = generator_input(INPUT);
+        assert_eq!(27, input.numbers.len());
+        assert_eq!(7, input.numbers[0]);
+        assert_eq!(Some(&1), input.numbers.iter().last());
+        let b = input.boards.iter().last().unwrap();
+        assert_eq!(3, input.boards.len());
+        assert_eq!(vec![10, 16, 15, 9, 19], b.data.row_iter(1).cloned().collect::<Vec<u8>>());
+        assert_eq!(vec![17, 15, 23, 13, 12], b.data.column_iter(2).cloned().collect::<Vec<u8>>());
+    }
+
+    #[test]
+    fn test_play() {
+        let input = generator_input(INPUT);
+        let b = input.boards.iter().last().unwrap();
+        assert_eq!(Some((11, 188)), b.play(&input.numbers));
+        for b in input.boards {
+            println!("{:?}", b.play(&input.numbers))
         }
     }
-    assert_eq!(Some(4512), part1(&parsed));
-    assert_eq!(Some(1924), part2(&parsed));
+
+    #[test]
+    fn test_part1() {
+        let input = generator_input(INPUT);
+        assert_eq!(4512, part1(&input));
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = generator_input(INPUT);
+        assert_eq!(1924, part2(&input));
+    }
 }
+
